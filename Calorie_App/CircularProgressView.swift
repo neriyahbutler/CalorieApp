da9c128522adaf02a3406531886f9cbe523fd027
Neriyah Butler <neriyahbutler21@gmail.com>
2021-10-19 09:38:55 -0500

    Sets up UI design and starts on circular progress bar

---
+++ Calorie_App/CircularProgressView.swift
@@ -0,0 +1,20 @@
+//
+//  CircularProgressView.swift
+//  Calorie_App
+//
+//  Created by student on 10/19/21.
+//
+
+import UIKit
+
+class CircularProgressView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Calorie_App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calorie_App
+//
+//  Created by student on 10/18/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    let test = 0;
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
