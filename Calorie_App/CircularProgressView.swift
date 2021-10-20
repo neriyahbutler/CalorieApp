3ad732a6771d775efca7d3c4396c4c76cfb853b5
Neriyah Butler <neriyahbutler21@gmail.com>
2021-10-20 01:13:56 -0500

    Adds circular progress bar, missing few details

--- Calorie_App/CircularProgressView.swift
+++ Calorie_App/CircularProgressView.swift
@@ -8,7 +8,12 @@
 import UIKit
 
 class CircularProgressView: UIView {
-
+    fileprivate var progressLayer = CAShapeLayer()
+    fileprivate var trackLayer = CAShapeLayer()
+    
+    var percentValue = 1
+    var percentTextValue = 0.4 * 100
+    
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
@@ -16,5 +21,57 @@ class CircularProgressView: UIView {
         // Drawing code
     }
     */
-
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        createCircularPath()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        createCircularPath()
+    }
+    
+    var progressColor = UIColor.white {
+        didSet {
+            progressLayer.strokeColor = progressColor.cgColor
+        }
+    }
+    
+    var trackColor = UIColor.white {
+        didSet {
+            trackLayer.strokeColor = trackColor.cgColor
+        }
+    }
+            
+    fileprivate func createCircularPath() {
+        
+        self.backgroundColor = UIColor.clear
+        self.layer.cornerRadius = self.frame.size.width/2
+        let circlePath = UIBezierPath(arcCenter: CGPoint(x: frame.size.width/2, y: frame.size.height/2), radius: (frame.size.width - 1.5)/2, startAngle: CGFloat(-0.5 * .pi), endAngle: CGFloat(1.5 * .pi), clockwise: true)
+        trackLayer.path = circlePath.cgPath
+        trackLayer.fillColor = CGColor(red: 237.0/255.0, green: 237.0/255.0, blue: 237.0/255.0, alpha: 1.0)
+        trackLayer.strokeColor = trackColor.cgColor
+        trackLayer.lineWidth = 10.0
+        trackLayer.strokeEnd = 1.0
+        layer.addSublayer(trackLayer)
+        
+        progressLayer.path = circlePath.cgPath
+        progressLayer.fillColor = UIColor.clear.cgColor
+        progressLayer.borderWidth = 9
+        progressLayer.borderColor = CGColor(red: 125.0/255.0, green: 195.0/255.0, blue: 131.0/255.0, alpha: 1.0)
+        progressLayer.strokeColor = progressColor.cgColor
+        progressLayer.lineWidth = 10.0
+        progressLayer.strokeEnd = CGFloat(Float(self.percentValue))
+        layer.addSublayer(progressLayer)
+    }
+    
+    func setProgressWithAnimation(duration: TimeInterval, value: Float) {
+        let animation = CABasicAnimation(keyPath: "strokeEnd")
+        animation.duration = duration
+        animation.fromValue = 0
+        animation.toValue = value
+        animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.linear)
+        progressLayer.strokeEnd = CGFloat(value)
+        progressLayer.add(animation, forKey: "animateprogress")
+    }
 }
--- Calorie_App/ProfileViewController.swift
+++ Calorie_App/ProfileViewController.swift
@@ -9,10 +9,32 @@ import UIKit
 
 class ProfileViewController: UIViewController {
     
+    @IBOutlet weak var percentLabel: UILabel!
+    @IBOutlet weak var CircularProgress: CircularProgressView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+//        let cp = CircularProgressView(frame: CGRect(x: 10.0, y: 10.0, width: 100.0, height: 100.0))
+//        cp.trackColor = UIColor.red
+//        cp.progressColor = UIColor.yellow
+//        cp.tag = 101
+//        self.view.addSubview(cp)
+//        cp.center = self.view.center
+//
+//        self.perform(#selector(animateProgress), with: nil, afterDelay: 2.0)
+        CircularProgress.trackColor = UIColor(red: 237.0/255.0, green: 237.0/255.0, blue: 237.0/255.0, alpha: 1.0)
+        CircularProgress.progressColor = UIColor(red: 157.0/255.0, green: 237.0/255.0, blue: 164.0/255.0, alpha: 1.0)
+        CircularProgress.setProgressWithAnimation(duration: 1.0, value: 0.6)
+        
+        percentLabel.text = String(CircularProgress.percentTextValue) + "%"
+        print(percentLabel.text)
     }
 
+    @objc func animateProgress() {
+        let cp = self.view.viewWithTag(101) as! CircularProgressView
+        cp.setProgressWithAnimation(duration: 1.0, value: 0.7)
+    }
+    
     /*
     // MARK: - Navigation
 
